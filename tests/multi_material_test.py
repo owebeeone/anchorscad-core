49231b8e1bc25ca2cc81d148e6ab4a9a009972fc
Gianni Mariani <gianni@mariani.ws>
2025-04-20 20:28:12 +1000

    Fixed the documentation related to multi-materials/multi-parts

--- tests/multi_material_test.py
+++ tests/multi_material_test.py
@@ -62,7 +62,7 @@ class MultiMaterialTest(ad.CompositeShape):
 
 # Uncomment the line below to default to writing OpenSCAD files
 # when anchorscad_main is run with no --write or --no-write options.
-MAIN_DEFAULT=ad.ModuleDefault(all=False)
+MAIN_DEFAULT=ad.ModuleDefault(all=True)
 
 if __name__ == "__main__":
     ad.anchorscad_main()
